06d0d781cd64256bfd68fafd0874cc1e9d612541
Daniel Andreas Wang <codingforpeace@protonmail.com>
2026-02-25 22:16:07 +0100

    add dropdown

--- src/components/Quizzer.vue
+++ src/components/Quizzer.vue
@@ -1,13 +1,24 @@
 <script setup lang="ts">
-import { onMounted } from 'vue';
+import { ref, watch } from 'vue';
 import { useQuizStore } from '../stores/quizStore';
 
 const quizStore = useQuizStore();
+const selectedTopic = ref('');
 
-onMounted(() => {
-  quizStore.fetchQuestions();
+watch(selectedTopic, (newTopic) => {
+  if (newTopic) {
+    quizStore.fetchQuestions(newTopic);
+  }
 });
 </script>
 
 <template>
+  <label for="topic">Select a topic:</label>
+  <select id="topic" v-model="selectedTopic">
+    <option value="">-- Please choose an option --</option>
+    <option value="general">General Knowledge</option>
+    <option value="math">Math</option>
+    <option value="science">Science</option>
+    <option value="history">History</option>
+  </select>
 </template>
