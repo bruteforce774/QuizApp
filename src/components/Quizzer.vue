c8dc08292ca7416a9181768c04dcc78eac43ea10
Daniel Andreas Wang <codingforpeace@protonmail.com>
2026-02-25 22:03:42 +0100

    fetch questions from store

--- src/components/Quizzer.vue
+++ src/components/Quizzer.vue
@@ -1,4 +1,12 @@
 <script setup lang="ts">
+import { onMounted } from 'vue';
+import { useQuizStore } from '../stores/quizStore';
+
+const quizStore = useQuizStore();
+
+onMounted(() => {
+  quizStore.fetchQuestions();
+});
 </script>
 
 <template>
